ce2aea10512662510919a82d322dba08286cbee0
rbxo0128 <rbxo0128@naver.com>
2025-04-12 22:28:15 +0900

    fix: cors 임시 수정

--- src/main/java/com/example/predict_match/config/SecurityConfig.java
+++ src/main/java/com/example/predict_match/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -32,6 +33,8 @@ public class SecurityConfig {
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
+                .headers(headers -> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .formLogin(form -> form
                         .loginPage("/login")
                         .loginProcessingUrl("/login-process") // 다른 URL 사용
